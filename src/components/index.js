7c85e426ee9da1bb8a17ad4e03624cff8b156240
salonigoyal2309 <salonigoyal2309@gmail.com>
2020-10-07 19:49:46 +0530

    index.js final

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './home.js'
 import Profile from './profile.js'
 import Projects from './projects.js'
 import Contact from './contact.js'
+import TopBar from './topbar.js'
+import '../styles/style.css'
 
 function Index(){
   return(
-  <div>
-    <Home />
-    <Profile />
-    <Projects />
-    <Contact />
-  </div>
+    <>
+      <Router>
+        <TopBar />
+        <Route path="/profile" component={Profile} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/" component={Home} />
+      </Router>
+    </>
   )
 }
 export default Index
