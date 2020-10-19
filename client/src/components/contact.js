9013d40eb2d3424c84d529fb0d1de0d29ae02676
salonigoyal2309 <salonigoyal2309@gmail.com>
2020-10-19 22:52:11 +0530

    server added

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import '../styles/style.css';
 import { Typography } from '@material-ui/core';
 import {SiLinkedin, SiGithub, SiInstagram, SiGmail, SiTwitter} from "react-icons/si";
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 const useStyles = makeStyles({
   root: {
@@ -13,35 +14,54 @@ const useStyles = makeStyles({
     width: '100vw',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 60,
+    paddingTop: 30,
   }
 });
 
+function sendMail(name,email,remarks) {
+    axios.get(`http://localhost:5000/api/mail`, {
+        params:{
+            name: name,
+            email: email,
+            remarks: remarks
+        }
+    },{
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        }
+    })
+}
+
 export default function Contact() {
   const classes = useStyles();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [remarks, setRemarks] = useState('');
 
   return (
     <div className={classes.root}>
         <Typography variant={'h4'}  style={{ fontFamily: 'Varela' , margin : 40, fontWeight: 'bold'}}>
             Contact
         </Typography>
-        <form>
+        <form onSubmit={(e)=>{sendMail(name,email,remarks)}}>
             <label style={{fontWeight:"bold"}}> Name
-                <input type="text" name="name" style={{marginLeft: 10 , marginRight: 50, borderRadius: 4}}/>
+                <input type="text" name="name" onChange={(e)=>setName(e.target.value)} style={{marginLeft: 10 , marginRight: 50, borderRadius: 4}}/>
             </label>
             <label style={{fontWeight:"bold"}}> Email
-                <input type="email" name="email" style={{margin: 10, marginRight: 50, borderRadius: 4}}/>
+                <input type="email" name="email" onChange={(e)=>setEmail(e.target.value)} style={{margin: 10, marginRight: 50, borderRadius: 4}}/>
             </label>
             <br/>
-            <textarea type="text" name="remarks" placeholder="Anything you want to say" rows="5" cols="40"
+            <textarea type="text" name="remarks" onChange={(e)=>setRemarks(e.target.value)} placeholder="Anything you want to say" rows="5" cols="40"
                 style={{margin: 10, marginRight: 50, borderRadius: 4, border: 'solid'}}/>
             <br />
-            <Button variant="outlined" style={{border:'solid'}}>Send</Button>
+            <Button type="submit" variant="outlined" style={{border:'solid'}}>Send</Button>
         </form>
         <div style={{backgroundColor:'black', marginTop: 60 , padding: 50}}>
             <a href="https://www.linkedin.com/in/saloni-goyal-085bba180" target="_blank" rel="noopener noreferrer">
                 <SiLinkedin id="LinkedIn" color="white" size="1.5em" />
             </a>
+
             <a href="https://github.com/salonigoyal2309" target="_blank" rel="noopener noreferrer">
                 <SiGithub id="Github" color="white" size="1.5em"/>
             </a>
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config()
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    next();
+  });
+
+app.get('/api/mail', (req, res) => {
+    const msg = {
+        to: process.env.PERSONAL_EMAIL,
+        from: process.env.PERSONAL_EMAIL,
+        cc: '2017ucp1061@mnit.ac.in',
+        subject: req.query.name + " viewed your personal website",
+        html: `<b>${ req.query.name }</b> with email <b>${ req.query.email }</b> viewed your personal website.<br />
+                Remarks: ${ req.query.remarks }`,
+    };
+    sgMail.send(msg);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
