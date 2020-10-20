04dfb0992b7cad215a731c3256fb1e4da01ba4f7
salonigoyal2309 <salonigoyal2309@gmail.com>
2020-10-20 19:46:36 +0530

    Updates

---
+++ precache-manifest.bcc81526f412778f73312deb59454611.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87dbe31124bd42ae88a090b466d1358",
+    "url": "/Portfolio-website/index.html"
+  },
+  {
+    "revision": "e220df42623ac7b9a655",
+    "url": "/Portfolio-website/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ac75356a30cd385017e1",
+    "url": "/Portfolio-website/static/css/main.dbd1b42c.chunk.css"
+  },
+  {
+    "revision": "e220df42623ac7b9a655",
+    "url": "/Portfolio-website/static/js/2.9366bfef.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/Portfolio-website/static/js/2.9366bfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac75356a30cd385017e1",
+    "url": "/Portfolio-website/static/js/main.e499e0c2.chunk.js"
+  },
+  {
+    "revision": "883932d2ccdd7cb5ba06",
+    "url": "/Portfolio-website/static/js/runtime-main.d09b9042.js"
+  },
+  {
+    "revision": "3fbfcbcee8f84a0f0fd9ccf5e4c6250b",
+    "url": "/Portfolio-website/static/media/attendance.3fbfcbce.png"
+  },
+  {
+    "revision": "2e6fa5e12ec30623c8535555070c23c5",
+    "url": "/Portfolio-website/static/media/codeInfo.2e6fa5e1.jpg"
+  },
+  {
+    "revision": "731f0fdbfb4b3a158fbd78c8736c4ff4",
+    "url": "/Portfolio-website/static/media/creditCard.731f0fdb.jpg"
+  },
+  {
+    "revision": "fa4d9c729bcc5897682e4e5110a3a9c0",
+    "url": "/Portfolio-website/static/media/openSource.fa4d9c72.jpg"
+  },
+  {
+    "revision": "38192657ce8cc241f8a5f3c6bae9f544",
+    "url": "/Portfolio-website/static/media/saloniCircle2.38192657.png"
+  },
+  {
+    "revision": "e8e892a714dc9a978eb827d9aa32a31d",
+    "url": "/Portfolio-website/static/media/shop.e8e892a7.jpg"
+  },
+  {
+    "revision": "bc17123bc581dd764d7f1d96898aaa3e",
+    "url": "/Portfolio-website/static/media/stationary.bc17123b.jpg"
+  }
+]);
